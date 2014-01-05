418a5041ad26b1b6c706f3eb9fb5a8693c36896e
Rick Richardson <rick.richardson@gmail.com>
2014-01-04 20:47:24 -0500

    initial commit

---
+++ init.el
@@ -0,0 +1,136 @@
+;;; better-defaults.el --- Fixing weird quirks and poor defaults
+
+;; Copyright © 2013 Phil Hagelberg
+
+;; Author: Phil Hagelberg
+;; URL: https://github.com/technomancy/better-defaults
+;; Version: 0.1.2
+;; Created: 2013-04-16
+;; Keywords: convenience
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Commentary:
+
+;; There are a number of unfortunate facts about the way Emacs works
+;; out of the box. While all users will eventually need to learn their
+;; way around in order to customize it to their particular tastes,
+;; this package attempts to address the most obvious of deficiencies
+;; in uncontroversial ways that nearly everyone can agree upon.
+
+;; Obviously there are many further tweaks you could do to improve
+;; Emacs, (like those the Starter Kit and similar packages) but this
+;; package focuses only on those that have near-universal appeal.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+;;;###autoload
+(progn
+  (ido-mode t)
+  (setq ido-enable-flex-matching t)
+
+  (menu-bar-mode -1)
+  (when (fboundp 'tool-bar-mode)
+    (tool-bar-mode -1))
+  (when (fboundp 'scroll-bar-mode)
+    (scroll-bar-mode -1))
+
+  (require 'uniquify)
+  (setq uniquify-buffer-name-style 'forward)
+
+  (require 'saveplace)
+  (setq-default save-place t)
+
+  (global-set-key (kbd "M-/") 'hippie-expand)
+  (global-set-key (kbd "C-x C-b") 'ibuffer)
+
+  (global-set-key (kbd "C-s") 'isearch-forward-regexp)
+  (global-set-key (kbd "C-r") 'isearch-backward-regexp)
+  (global-set-key (kbd "C-M-s") 'isearch-forward)
+  (global-set-key (kbd "C-M-r") 'isearch-backward)
+
+  (show-paren-mode 1)
+  (setq-default indent-tabs-mode nil)
+  (setq x-select-enable-clipboard t
+        x-select-enable-primary t
+        save-interprogram-paste-before-kill t
+        apropos-do-all t
+        mouse-yank-at-point t
+        save-place-file (concat user-emacs-directory "places")
+        backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                                 "backups")))))
+
+
+(provide 'better-defaults)
+;;; better-defaults.el ends here
+
+
+(require 'package)
+(add-to-list 'package-archives
+              '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+;;(package-refresh-contents)
+
+(add-to-list 'load-path "~/.emacs.d/cider/")
+(require 'cider)
+
+(defvar my-packages '(starter-kit
+                       starter-kit-lisp
+                       starter-kit-bindings
+                       starter-kit-eshell
+                       clojure-mode
+                       clojurescript-mode
+                       color-theme
+                       clojure-test-mode))
+
+ (dolist (p my-packages)
+   (when (not (package-installed-p p))
+     (package-install p)))
+
+(setq cider-popup-stacktraces nil)
+
+(require 'color-theme)
+(load-file "~/.emacs.d/twilight-emacs/color-theme-twilight.el")
+(color-theme-twilight)
+
+(global-linum-mode 1)
+
+(load-file "~/.emacs.d/strtokeyw.el")
+(global-set-key (kbd "C-:") 'toggle-clj-keyword-string)
+
+(defun shift-region (distance)
+  (let ((mark (mark)))
+    (save-excursion
+      (indent-rigidly (region-beginning) (region-end) distance)
+      (push-mark mark t t)
+      ;; Tell the command loop not to deactivate the mark
+      ;; for transient mark mode
+      (setq deactivate-mark nil))))
+
+(defun shift-right ()
+  (interactive)
+  (shift-region 1))
+
+(defun shift-left ()
+  (interactive)
+  (shift-region -1))
+
+(global-set-key [C-S-right] 'shift-right)
+(global-set-key [C-S-left] 'shift-left)
